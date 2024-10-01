cb4928e0c18d263e061c716ea599720266a1902f
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-01 19:09:24 +0300

    add retries

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import cron from 'node-cron';
 import express from 'express';
 import config from './config/configuration';
 import startOdinOnline from '../scripts/odinOnlineBot';
+import runWithRetries from './utils/runWithRetries';
 
 const { monday, tuesday, wednesday, thursday, friday } = config.schedule;
 
@@ -10,78 +11,78 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 cron.schedule('0 9 * * 1', async () => {
-    await startOdinOnline();
-    await startZoomClass(monday.first, 1);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(monday.first, 1));
 });
 
 cron.schedule('45 10 * * 1', async () => {
-    await startOdinOnline();
+    await runWithRetries(startOdinOnline);
     await startZoomClass(monday.second, 2);
 });
 
 cron.schedule('45 12 * * 1', async () => {
-    await startOdinOnline();
-    await startZoomClass(monday.third, 3);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(monday.third, 3));
 });
 
 cron.schedule('30 14 * * 1', async () => {
-    await startOdinOnline();
-    await startZoomClass(monday.four, 4);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(monday.four, 4));
 });
 
 cron.schedule('0 9 * * 2', async () => {
-    await startOdinOnline();
-    await startZoomClass(tuesday.first, 1);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(tuesday.first, 1));
 });
 
 cron.schedule('45 10 * * 2', async () => {
-    await startOdinOnline();
-    await startZoomClass(tuesday.second, 2);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(tuesday.second, 2));
 });
 
 cron.schedule('0 9 * * 3', async () => {
-    await startOdinOnline();
-    await startZoomClass(wednesday.first, 1);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(wednesday.first, 1));
 });
 
 cron.schedule('45 10 * * 3', async () => {
-    await startOdinOnline();
-    await startZoomClass(wednesday.second, 2);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(wednesday.second, 2));
 });
 
 cron.schedule('45 12 * * 3', async () => {
-    await startOdinOnline();
-    await startZoomClass(wednesday.third, 3);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(wednesday.third, 3));
 });
 
 cron.schedule('30 14 * * 4', async () => {
-    await startOdinOnline();
-    await startZoomClass(thursday.first, 1);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(thursday.first, 1));
 });
 
 cron.schedule('15 16 * * 4', async () => {
-    await startOdinOnline();
-    await startZoomClass(thursday.second, 2);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(thursday.second, 2));
 });
 
 cron.schedule('45 12 * * 5', async () => {
-    await startOdinOnline();
-    await startZoomClass(friday.first, 1);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(friday.first, 1));
 });
 
 cron.schedule('30 14 * * 5', async () => {
-    await startOdinOnline();
-    await startZoomClass(friday.second, 2);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(friday.second, 2));
 });
 
 cron.schedule('15 16 * * 5', async () => {
-    await startOdinOnline();
-    await startZoomClass(friday.third, 3);
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(friday.third, 3));
 });
 
-cron.schedule('39 0 * * 1', async () => {
-    await startOdinOnline();
-    await startZoomClass('https://us05web.zoom.us/j/89193547230?pwd=SXxhDEUTIVb8gYODlhRKyQRtfthPc1.1', 3);
+cron.schedule('8 19 * * 2', async () => {
+    await runWithRetries(startOdinOnline);
+    await runWithRetries(() => startZoomClass(friday.third, 3));
 });
 
 app.listen(PORT, () => {
