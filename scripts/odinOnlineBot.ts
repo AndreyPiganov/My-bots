b39861cc8ca89ae96967f151648bf2f1c935c5a2
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-01 19:10:44 +0300

    add try/catch

--- scripts/odinOnlineBot.ts
+++ scripts/odinOnlineBot.ts
@@ -5,40 +5,45 @@ import getCurrentDay from '../src/utils/getCurrentDay';
 import logger from '../src/utils/logger';
 
 export default async function startOdinOnline(): Promise<void> {
-    const now = new Date();
-    const dayOfWeek = getCurrentDay();
-    const time = formatTime(now);
-    logger.info(`Бот заходит для показательного онлайна в ${dayOfWeek} ${time}`);
+    try {
+        const now = new Date();
+        const dayOfWeek = getCurrentDay();
+        const time = formatTime(now);
+        logger.info(`Бот заходит для показательного онлайна в ${dayOfWeek} ${time}`);
 
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--allow-file-access'],
-        userDataDir: './session/odin'
-    });
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: ['--allow-file-access'],
+            userDataDir: './session/odin'
+        });
 
-    const page = await browser.newPage();
+        const page = await browser.newPage();
 
-    await page.setViewport({
-        width: 1280,
-        height: 720,
-        deviceScaleFactor: 1
-    });
+        await page.setViewport({
+            width: 1280,
+            height: 720,
+            deviceScaleFactor: 1
+        });
 
-    await page.goto('https://www.odin.study/ru/Account/Login', { waitUntil: 'networkidle2' });
+        await page.goto('https://www.odin.study/ru/Account/Login', { waitUntil: 'networkidle2' });
 
-    const isLoggin = await page.$('button[data-v-9915af7a]');
+        const isLoggin = await page.$('button[data-v-9915af7a]');
 
-    if (isLoggin) {
-        await page.type('input[type="email"]', config.odin.email);
+        if (isLoggin) {
+            await page.type('input[type="email"]', config.odin.email);
 
-        await page.type('input[type="password"]', config.odin.password);
+            await page.type('input[type="password"]', config.odin.password);
 
-        await isLoggin.click();
+            await isLoggin.click();
+        }
+
+        const endDateWork = new Date();
+
+        logger.info(`Бот закончил работу в ${dayOfWeek} - ${formatTime(endDateWork)}`);
+
+        await browser.close();
+    } catch (error) {
+        logger.error('Произошла ошибка', error);
+        throw error;
     }
-
-    const endDateWork = new Date();
-
-    logger.info(`Бот закончил работу в ${dayOfWeek} - ${formatTime(endDateWork)}`);
-
-    await browser.close();
 }
--- scripts/runZoomBot.ts
+++ scripts/runZoomBot.ts
@@ -4,106 +4,111 @@ import getCurrentDay from '../src/utils/getCurrentDay';
 import logger from '../src/utils/logger';
 
 export default async function startZoomClass(link: string, numberClass: number): Promise<void> {
-    const now = new Date();
-    const dayOfWeek = getCurrentDay();
-    const time = formatTime(now);
-
-    logger.info(`Выполняется задача в ${dayOfWeek} ${time} - ${numberClass} пара`);
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-            '--use-fake-ui-for-media-stream', // Автоматически принимает запросы на доступ к микрофону/камере
-            '--use-fake-device-for-media-stream', // Использует фейковые устройства для тестирования
-            '--use-file-for-fake-audio-capture=./assets/micro.wav',
-            '--allow-file-access'
-        ],
-        ignoreDefaultArgs: ['--mute-audio']
-    });
-
-    const context = browser.defaultBrowserContext();
-
-    await context.overridePermissions(link, ['microphone', 'camera']);
-    await context.overridePermissions('https://zoom.us', ['microphone', 'camera']);
-    await context.overridePermissions('https://app.zoom.us', ['microphone', 'camera']);
-
-    const page = await browser.newPage();
-
-    const page2 = await browser.newPage(); // Для скипа окна браузера зума.
-
-    await page.setViewport({
-        width: 1280,
-        height: 720,
-        deviceScaleFactor: 1 // Масштабирование. 1 = 100%, 2 = 200%, и т.д.
-    });
-
-    await page2.setViewport({
-        width: 1280,
-        height: 720,
-        deviceScaleFactor: 1
-    });
-
-    await page.goto(link);
-    await page2.goto(link);
-
-    await page2.close();
-
-    await page.waitForSelector('a[download]', { timeout: 60000 });
-
-    await page.click('a[download]');
-
-    await page.waitForSelector('a[web_client]');
-
-    const response = await Promise.all([
-        page.waitForNavigation(), // Ожидание редиректа
-        page.click('a[web_client]') // Клик по элементу
-    ]);
-
-    await page.goto(response[0].url());
-
-    const iframeElement = await page.$('iframe');
-
-    const frame = await iframeElement.contentFrame();
-
-    await frame.waitForSelector('input[type="text"]', { visible: true });
-
-    await frame.type('input[type="text"]', 'Пиганов Андрей 3102д');
-
-    await frame.click('button[type=button]');
     try {
-        await frame.waitForSelector('div[class="preview-video__control-button-container simple"]', {
-            timeout: 5000
+        const now = new Date();
+        const dayOfWeek = getCurrentDay();
+        const time = formatTime(now);
+
+        logger.info(`Выполняется задача в ${dayOfWeek} ${time} - ${numberClass} пара`);
+
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                '--use-fake-ui-for-media-stream', // Автоматически принимает запросы на доступ к микрофону/камере
+                '--use-fake-device-for-media-stream', // Использует фейковые устройства для тестирования
+                '--use-file-for-fake-audio-capture=./assets/micro.wav',
+                '--allow-file-access'
+            ],
+            ignoreDefaultArgs: ['--mute-audio']
         });
 
-        await frame.click('div[class="preview-video__control-button-container simple"]');
+        const context = browser.defaultBrowserContext();
 
-        await frame.click('div[class="preview-video__control-button-container simple"]');
+        await context.overridePermissions(link, ['microphone', 'camera']);
+        await context.overridePermissions('https://zoom.us', ['microphone', 'camera']);
+        await context.overridePermissions('https://app.zoom.us', ['microphone', 'camera']);
 
-        logger.info('Отлично подключили звук для конференции');
-    } catch {
-        await frame.waitForSelector(
-            'button[class="zm-btn join-audio-by-voip__join-btn zm-btn--primary zm-btn__outline--white zm-btn--lg"]'
-        );
+        const page = await browser.newPage();
 
-        await frame.click(
-            'button[class="zm-btn join-audio-by-voip__join-btn zm-btn--primary zm-btn__outline--white zm-btn--lg"]'
-        );
+        const page2 = await browser.newPage(); // Для скипа окна браузера зума.
 
-        logger.warn('Конференция уже работает и подключили звук');
+        await page.setViewport({
+            width: 1280,
+            height: 720,
+            deviceScaleFactor: 1 // Масштабирование. 1 = 100%, 2 = 200%, и т.д.
+        });
+
+        await page2.setViewport({
+            width: 1280,
+            height: 720,
+            deviceScaleFactor: 1
+        });
+
+        await page.goto(link);
+        await page2.goto(link);
+
+        await page2.close();
+
+        await page.waitForSelector('a[download]', { timeout: 60000 });
+
+        await page.click('a[download]');
+
+        await page.waitForSelector('a[web_client]');
+
+        const response = await Promise.all([
+            page.waitForNavigation(), // Ожидание редиректа
+            page.click('a[web_client]') // Клик по элементу
+        ]);
+
+        await page.goto(response[0].url());
+
+        const iframeElement = await page.$('iframe');
+
+        const frame = await iframeElement.contentFrame();
+
+        await frame.waitForSelector('input[type="text"]', { visible: true });
+
+        await frame.type('input[type="text"]', 'Пиганов Андрей 3102д');
+
+        await frame.click('button[type=button]');
+        try {
+            await frame.waitForSelector('div[class="preview-video__control-button-container simple"]', {
+                timeout: 5000
+            });
+
+            await frame.click('div[class="preview-video__control-button-container simple"]');
+
+            await frame.click('div[class="preview-video__control-button-container simple"]');
+
+            logger.info('Отлично подключили звук для конференции');
+        } catch {
+            await frame.waitForSelector(
+                'button[class="zm-btn join-audio-by-voip__join-btn zm-btn--primary zm-btn__outline--white zm-btn--lg"]'
+            );
+
+            await frame.click(
+                'button[class="zm-btn join-audio-by-voip__join-btn zm-btn--primary zm-btn__outline--white zm-btn--lg"]'
+            );
+
+            logger.warn('Конференция уже работает и подключили звук');
+        }
+
+        logger.info('Бот вошел в конференцию');
+
+        await frame.waitForSelector('button[class="zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue"]', {
+            timeout: 0
+        });
+
+        await frame.click('button[class="zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue"]');
+
+        const endDateWork = new Date();
+        const endTime = formatTime(endDateWork);
+
+        logger.info(`Задача закончилась в ${dayOfWeek} ${endTime}`);
+
+        await browser.close();
+    } catch (error) {
+        logger.error('Произошла ошибка', error);
+        throw error;
     }
-
-    logger.info('Бот вошел в конференцию');
-
-    await frame.waitForSelector('button[class="zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue"]', {
-        timeout: 0
-    });
-
-    await frame.click('button[class="zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue"]');
-
-    const endDateWork = new Date();
-    const endTime = formatTime(endDateWork);
-
-    logger.info(`Задача закончилась в ${dayOfWeek} ${endTime}`);
-
-    await browser.close();
 }
