a827f172a4b922c0985c38c1efe6f445ac2be0e6
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-02 18:28:52 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,7 @@ cron.schedule('15 16 * * 5', async () => {
     await runWithRetries(() => startZoomClass(friday.third, 3));
 });
 
-cron.schedule('27 18 * * 3', async () => {
+cron.schedule('31 18 * * 3', async () => {
     // Тестовый schedule
     await runWithRetries(startOdinOnline);
     await runWithRetries(() => startZoomClass(friday.third, 3));
