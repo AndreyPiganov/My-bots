6b525a2741e3b6b335ca947ab56b55dec632225f
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-09 19:49:30 +0300

    fix

--- scripts/hhBot.ts
+++ scripts/hhBot.ts
@@ -31,6 +31,10 @@ export default async function runHHBot(): Promise<void> {
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
         );
 
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'en-US,en;q=0.9'
+        });
+
         await page.setViewport({
             width: 1280,
             height: 720,
