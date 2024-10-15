e8f1dc4180cf841134f6bb9f1e874b02db25fbfb
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-15 18:37:43 +0300

    fix

--- scripts/hhBot.ts
+++ scripts/hhBot.ts
@@ -91,7 +91,7 @@ export default async function runHHBot(): Promise<void> {
         return;
     } catch (error) {
         logger.error('Произошла ошибка', error);
-        const sessionDirRm = path.resolve(__dirname, 'session/hh');
+        const sessionDirRm = path.resolve('~', 'session/hh');
         if (fs.existsSync(sessionDirRm)) {
             logger.info(`Удаление папки: ${sessionDirRm}`);
             fs.rm(sessionDirRm, { recursive: true, force: true }, (err) => {
