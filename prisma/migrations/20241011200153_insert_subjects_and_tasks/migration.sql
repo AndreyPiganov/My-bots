45a72b34e9a9fb372a09a7d98fb2046477011b29
AndreyPiganov <andrey.piganov@mail.ru>
2024-10-18 17:48:41 +0300

    fix

--- prisma/migrations/20241011200153_insert_subjects_and_tasks/migration.sql
+++ prisma/migrations/20241011200153_insert_subjects_and_tasks/migration.sql
@@ -8,9 +8,9 @@ VALUES
 ('Технология разработки и защиты баз данных', 'https://zoom.us/j/7463860716?pwd=NTJnMXIrMHlPRnlCUEJSZlR1cmVCdz09', NOW(), NOW()),
 ('Психология общения', 'https://zoom.us/j/2615158278?pwd=Vnk0L3EycnVUeTNFYlJ2RWZxNURJQT09', NOW(), NOW()),
 ('Системное программирование Node.js', 'https://zoom.us/j/4095516819?pwd=SlQyQ2YvRWZ1OE1Qdm5UamVTRk5KUT09', NOW(), NOW()),
-('Стандартизация, сертификация и техническое документоведение', 'https://zoom.us/j/6480814148?pwd=SjJaeWkvelg0Y3JWYmdsRG9veElLQT09', NOW(), NOW());
-('Правовое обеспечение профессиональной деятельности', 'https://zoom.us/j/8109183427?pwd=NTd4SG4zNHpwUVNDa3c5NVNWTFBEdz09', NOW(), NOW());
-('Второй диплом', 'https://zoom.us/j/6480814148?pwd=SjJaeWkvelg0Y3JWYmdsRG9veElLQT09', NOW(), NOW());
+('Стандартизация, сертификация и техническое документоведение', 'https://zoom.us/j/6480814148?pwd=SjJaeWkvelg0Y3JWYmdsRG9veElLQT09', NOW(), NOW()),
+('Правовое обеспечение профессиональной деятельности', 'https://zoom.us/j/8109183427?pwd=NTd4SG4zNHpwUVNDa3c5NVNWTFBEdz09', NOW(), NOW()),
+('Второй диплом', 'https://zoom.us/j/6480814148?pwd=SjJaeWkvelg0Y3JWYmdsRG9veElLQT09', NOW(), NOW()),
 ('Тестовый блок', 'https://zoom.us/j/88300122006?pwd=ESgqACyqnHo88RDx1O3Kyd7Faq1j3k.1', NOW(), NOW());
 
 -- Понедельник
