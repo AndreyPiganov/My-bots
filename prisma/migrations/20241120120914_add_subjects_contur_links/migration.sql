24edbcfbd8980eb78239d0fd43ea1eef36b98728
AndreyPiganov <andrey.piganov@mail.ru>
2024-11-20 15:32:40 +0300

    add new migration by konturLinks

---
+++ prisma/migrations/20241120120914_add_subjects_contur_links/migration.sql
@@ -0,0 +1,25 @@
+-- This is an empty migration.
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/oxcbvaqmhnh1'
+WHERE name = 'Безопасность жизнедеятельности';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/cx4omatjx2b0'
+WHERE name = 'Правовое обеспечение профессиональной деятельности';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/ala6r2xtpfr2'
+WHERE name = 'Психология общения';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/yv3aru2ap4a5'
+WHERE name = 'Разработка программных модулей';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/yv3aru2ap4a5'
+WHERE name = 'Системное программирование Node.js';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/yv3aru2ap4a5'
+WHERE name = 'Поддержка и тестирование программных модулей';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/q5ko1rchk6by'
+WHERE name = 'Технология разработки и защиты баз данных';
+
+UPDATE subjects SET "konturLink" = 'https://hexletcol2.ktalk.ru/xk1samrasxnh'
+WHERE name = 'Второй диплом';
